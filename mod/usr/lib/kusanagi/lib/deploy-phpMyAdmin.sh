8feb41efea422b9e1e10f7e05f82a8bb68c44379
kusanagi@dev.q.kotsu.do <dev@q.kotsu.do>
2019-01-14 16:44:17 +0900

    fix permission

--- mod/usr/lib/kusanagi/lib/deploy-phpMyAdmin.sh
+++ mod/usr/lib/kusanagi/lib/deploy-phpMyAdmin.sh
@@ -10,12 +10,12 @@ cp -r /usr/lib/kusanagi/skel/etc $TARGET_DIR/etc
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 yum -y install yarn
 
+chown -R kusanagi.kusanagi $TARGET_DIR
+chmod 0777 $TARGET_DIR/DocumentRoot
+
 cd $TARGET_DIR/DocumentRoot
 sudo -u kusanagi ${KUSANAGI_COMPOSER_BIN} update
 sudo -u kusanagi yarn install
 
 # 多言語化
 sudo -u kusanagi scripts/generate-mo
-
-chown -R kusanagi.kusanagi $TARGET_DIR
-chmod 0777 $TARGET_DIR/DocumentRoot
